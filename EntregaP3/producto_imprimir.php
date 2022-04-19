30f74c51a0f56bf88e20e479f1c6707600ca0c60
Santiago <santigilegaza@correo.ugr.es>
2022-04-19 16:14:50 +0200

    agrego los archivos para la entrega de la P3

---
+++ EntregaP3/producto_imprimir.php
@@ -0,0 +1,21 @@
+<?php
+  require_once "/usr/local/lib/php/vendor/autoload.php";
+  include("bd.php");
+  
+  $loader = new \Twig\Loader\FilesystemLoader('templates');
+  $twig = new \Twig\Environment($loader);
+  
+  if (isset($_GET['ev'])) {
+    $idEv = $_GET['ev'];
+  } else {
+    $idEv = -1;
+  }
+   
+  $producto = getProducto($idEv);
+  $numFotos= getNumFotos($idEv);
+  $fotos= getFotos($idEv);
+
+  $numFotos= getNumFotos($idEv);
+  
+  echo $twig->render('producto_imprimir.html', ['producto' => $producto, 'fotos' => $fotos, 'numReseñas' => $numReseñas, 'numFotos' => $numFotos]);
+?>
