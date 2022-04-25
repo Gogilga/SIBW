c743d7c96a2cf760ba1b067511d37f6defe2e8b9
Gogilga <santigilegaza@correo.ugr.es>
2022-04-25 19:59:43 +0200

    añado la práctica 4

---
+++ Practica4/index.php
@@ -0,0 +1,24 @@
+<?php
+  require_once "/usr/local/lib/php/vendor/autoload.php";
+  require_once 'bdUsuarios.php';
+  include("bd.php");
+  
+
+  $loader = new \Twig\Loader\FilesystemLoader('templates');
+  $twig = new \Twig\Environment($loader);
+
+  $numProductos= getNumProductos();
+
+  $productos= getProductosPortada($numProductos);
+
+  //Control de sesiones
+  $variablesParaTwig = [];
+  
+  session_start();
+  
+  if (isset($_SESSION['nickUsuario'])) {
+    $variablesParaTwig['user'] = getUser($_SESSION['nickUsuario']);
+  }
+
+  echo $twig->render('portada.html', ['productos' => $productos, 'numProductos' => $numProductos, 'variablesParaTwig' => $variablesParaTwig]);
+?>
---
+++ Practica4/producto.php
@@ -0,0 +1,35 @@
+<?php
+  require_once "/usr/local/lib/php/vendor/autoload.php";
+  require_once 'bdUsuarios.php';
+  include("bd.php");
+  
+  $loader = new \Twig\Loader\FilesystemLoader('templates');
+  $twig = new \Twig\Environment($loader);
+  
+  if (isset($_GET['ev'])) {
+    $idEv = $_GET['ev'];
+  } else {
+    $idEv = -1;
+  }
+   
+  $producto = getProducto($idEv);
+  $numFotos= getNumFotos($idEv);
+  $fotos= getFotos($idEv);
+
+  $reseñas= getReseñas($idEv);
+  $numReseñas= getNumReseñas($idEv);
+
+  //Para obtener las palabras censuradas
+  $censura= getCensura();
+
+  //Control de sesiones
+  $variablesParaTwig = [];
+  
+  session_start();
+  
+  if (isset($_SESSION['nickUsuario'])) {
+    $variablesParaTwig['user'] = getUser($_SESSION['nickUsuario']);
+  }
+  
+  echo $twig->render('producto.html', ['producto' => $producto, 'fotos' => $fotos, 'numReseñas' => $numReseñas, 'reseñas' => $reseñas, 'numFotos' => $numFotos, 'censura' => $censura, 'variablesParaTwig' => $variablesParaTwig]);
+?>
